7d0e6af4a82767490a241447e7f66dcef53be13f
Polashahmad01 <dev.polashahmad@gmail.com>
2024-07-14 23:16:06 +0600

    Add ToolsIUsePage component to HomePage.Implemented ToolsIUsePage component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import IntroPage from "@/components/IntroPage";
 import WhatIDoPage from "@/components/WhatIDoPage";
+import ToolIUsePage from "@/components/ToolsIUsePage";
 
 export default function HomePage() {
   return (
     <main className="container mx-auto">
       <IntroPage />
       <WhatIDoPage />
+      <ToolIUsePage />
     </main>
   );
 }
