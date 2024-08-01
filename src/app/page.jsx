018c16e73d0682fd0ff2a9a3417fdc483ace897c
Polashahmad01 <dev.polashahmad@gmail.com>
2024-08-01 13:54:26 +0600

    Update ContactPage layout and design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,8 +15,8 @@ export default function HomePage() {
       {/* <WhatIDoPage />
       <ToolsIUsePage />
       <MyRecentWorkPage />
-      <MyExperiencePage />
-      <ContactPage /> */}
+      <MyExperiencePage /> */}
+      <ContactPage />
       <AppFooter />
     </main>
   )
