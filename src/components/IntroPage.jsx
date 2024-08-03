d8dc35daed723855b91643e6e6e9408e32434222
Polashahmad01 <dev.polashahmad@gmail.com>
2024-08-03 22:52:44 +0600

    Update IntroPage layout and styling

--- src/components/IntroPage.jsx
+++ src/components/IntroPage.jsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-// import authorImgSrc from "../../public/images/polash.png";
 import realImgSrc from "../../public/images/polash_ahmad.jpg";
 import { Righteous } from "next/font/google";
 
@@ -7,40 +6,38 @@ const righteous = Righteous({ subsets: ["latin"], weight: ["400"] });
 
 export default function IntroPage() {
   return (
-    <section className="flex justify-center items-center min-h-screen">
-
-      <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center h-full">
-        <div className="flex-1">
-          <div className="block mx-auto object-cover w-64 sm:w-96">
-            <Image
-              // src={authorImgSrc}
-              src={realImgSrc}
-              alt="Polash Ahmad"
-              priority
-              className="rounded-2xl"
-            />
+    <section className="container mx-auto">
+      <div className="mx-4 xl:mx-0">
+        <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-8 pt-[10vh] lg:py-[20vh]">
+          <div>
+            <div className="block mx-auto object-cover w-64 sm:w-96">
+              <Image
+                src={realImgSrc}
+                alt="Polash Ahmad"
+                priority
+                className="rounded-2xl"
+              />
+            </div>
           </div>
-        </div>
-
-        <div className="flex-1 opacity-85">
-          <h1 className={`mb-4 leading-[61px] text-5xl ${righteous.className}`}>
-            <span className="">Hi there,</span>
-            <br />
-            <span className="">I&apos;m Polash Ahmad</span>
-          </h1>
-          <p className={`mb-4 ${righteous.className}`}>
-            Transforming ideas into visually stunning and high-performing web interfaces is my specialty.
-            With expertise in front-end development, I create solutions that blend elegance with efficiency.
-            Eye for design ensures pixel-perfect accuracy, while my commitment to innovation drives me to explore and implement the latest techniques.
-            Let&apos;s collaborate and bring your vision to life with exceptional results!
-          </p>
-          <div className="flex items-center gap-4 font-medium text-sm text-black opacity-85">
-            <button className="px-8 py-3 border border-solid rounded-xl backdrop-blur-xl border-[rgba(255,255,255,.1)] bg-white">Download CV</button>
-            <button className="px-8 py-3 border border-solid rounded-xl backdrop-blur-xl border-[rgba(255,255,255,.1)] bg-white">Contact</button>
+          <div className="opacity-85">
+            <h1 className={`mb-4 lg:leading-[61px] text-3xl text-center lg:text-left lg:text-5xl ${righteous.className}`}>
+              <span className="">Hi there,</span>
+              <br />
+              <span className="">I&apos;m Polash Ahmad</span>
+            </h1>
+            <p className={`mb-4 ${righteous.className} font-light text-center lg:text-left mb-5`}>
+              Transforming ideas into visually stunning and high-performing web interfaces is my specialty.
+              With expertise in front-end development, I create solutions that blend elegance with efficiency.
+              Eye for design ensures pixel-perfect accuracy, while my commitment to innovation drives me to explore and implement the latest techniques.
+              Let&apos;s collaborate and bring your vision to life with exceptional results!
+            </p>
+            <div className="text-sm flex items-center justify-center lg:justify-start gap-4">
+              <button className="bg-[#141414] py-3 px-6 rounded-md font-medium border border-solid backdrop-blur-xl border-[rgba(255,255,255,.1)] bg-[rgba(4,19,14,.5)]">Download</button>
+              <button className="bg-[#141414] py-3 px-6 rounded-md font-medium border border-solid backdrop-blur-xl border-[rgba(255,255,255,.1)] bg-[rgba(4,19,14,.5)]">Contact</button>
+            </div>
           </div>
         </div>
       </div>
-
     </section>
   );
 }
