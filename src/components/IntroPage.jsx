428be6fa8923b7aaaddb6bcdf3d370f43e9de3b6
Polashahmad01 <dev.polashahmad@gmail.com>
2024-08-07 18:26:08 +0600

    Update IntroPage styling and button design

--- src/components/IntroPage.jsx
+++ src/components/IntroPage.jsx
@@ -6,7 +6,7 @@ const righteous = Righteous({ subsets: ["latin"], weight: ["400"] });
 
 export default function IntroPage() {
   return (
-    <section className="container mx-auto">
+    <section className="container mx-auto pt-[4vh]">
       <div className="mx-4 xl:mx-0">
         <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-8 pt-[10vh] lg:py-[20vh]">
           <div>
@@ -21,19 +21,19 @@ export default function IntroPage() {
           </div>
           <div className="opacity-85">
             <h1 className={`mb-4 lg:leading-[61px] text-3xl text-center lg:text-left lg:text-5xl ${righteous.className}`}>
-              <span className="">Hi there,</span>
+              Hi there,
               <br />
-              <span className="">I&apos;m Polash Ahmad</span>
+              I&apos;m <span className="text-[#20bf55]">Polash Ahmad</span>
             </h1>
-            <p className={`mb-4 ${righteous.className} font-light text-center lg:text-left mb-5`}>
+            <p className="text-center lg:text-left mb-8">
               Transforming ideas into visually stunning and high-performing web interfaces is my specialty.
               With expertise in front-end development, I create solutions that blend elegance with efficiency.
               Eye for design ensures pixel-perfect accuracy, while my commitment to innovation drives me to explore and implement the latest techniques.
               Let&apos;s collaborate and bring your vision to life with exceptional results!
             </p>
-            <div className="text-sm flex items-center justify-center lg:justify-start gap-4">
-              <button className="bg-[#141414] py-3 px-6 rounded-md font-medium border border-solid backdrop-blur-xl border-[rgba(255,255,255,.1)] bg-[rgba(4,19,14,.5)]">Download</button>
-              <button className="bg-[#141414] py-3 px-6 rounded-md font-medium border border-solid backdrop-blur-xl border-[rgba(255,255,255,.1)] bg-[rgba(4,19,14,.5)]">Contact</button>
+            <div className="text-[13px] flex items-center justify-center lg:justify-start gap-4">
+              <button className="bg-[#171717] px-6 py-[14px] uppercase rounded-tr-xl rounded-bl-xl border border-solid border-[rgba(255,255,255,.1)] transition-all hover:border-[#20bf55] hover:rounded-xl hover:text-[#20bf55]">Download</button>
+              <button className="bg-[#171717] px-6 py-[14px] uppercase rounded-tr-xl rounded-bl-xl border border-solid border-[rgba(255,255,255,.1)] transition-all hover:border-[#20bf55] hover:rounded-xl hover:text-[#20bf55]">Contact</button>
             </div>
           </div>
         </div>
