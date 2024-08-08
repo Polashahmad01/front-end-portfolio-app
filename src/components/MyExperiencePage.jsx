9a4d7b50a545b335698a2d24b55b5e00f39ee716
Polashahmad01 <dev.polashahmad@gmail.com>
2024-08-08 17:30:36 +0600

    Update contact page layout and styles.- Refactor ContactPage component.- Add new styles and structure.- Improve user interaction

--- src/components/MyExperiencePage.jsx
+++ src/components/MyExperiencePage.jsx
@@ -11,7 +11,7 @@ const righteous = Righteous({ subsets: ["latin"], weight: ["400"] });
 export default function MyExperiencePage() {
   return (
     <section className="common-container">
-      <div className="px-4 xl:px-0">
+      <div className="px-4 xl:px-0 mb-[10vh]">
         <h3 className={`${righteous.className} text-3xl opacity-85 mb-[10vh] text-center lg:text-[56px]`}>My Experience</h3>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
           <div className="opacity-85 p-8 rounded-xl bg-[#171717]">
