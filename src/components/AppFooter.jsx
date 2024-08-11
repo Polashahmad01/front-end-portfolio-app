70469764287d55aabba76b673133b4b1be73a18d
Polashahmad01 <dev.polashahmad@gmail.com>
2024-08-11 14:25:44 +0600

    Update contact page design styles and button color

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,7 +1,7 @@
 
 export default function AppFooter() {
   return (
-    <footer className="border-t border-solid border-[#181818] font-light opacity-85 px-4 lg:px-0">
+    <footer className="border-t-[1px] border-solid border-[#454547b3] font-light opacity-60 px-4 lg:px-0">
       <div className="container mx-auto grid grid-rows-1 justify-center items-center">
         <p className="text-sm py-8">&copy; {new Date().getFullYear()} All rights reserved - PolashAhmad</p>
       </div>
--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -12,27 +12,27 @@ export default function ContactPage() {
         <h3 className={`${righteous.className} text-3xl opacity-85 mb-[10vh] text-center lg:text-[56px]`}>Contact Me</h3>
         <div className="grid grid-cols-1 sm:grid-cols-2 items-center gap-20 mx-4 xl:mx-0">
           <div className="opacity-85">
-            <div className="mb-6">
-              <Link href="mailto:dev.polashahmad@gmail.com" className={`px-4 py-[6px] uppercase text-sm border border-solid rounded-[80px] backdrop-blur-xl border-[rgba(255,255,255,.4)] bg-[rgba(4,19,14,.5)] transition-all hover:border-[#20bf55] hover:rounded-[80px] hover:text-[#20bf55]`}>Contact Me</Link>
+            <div className="mb-10">
+              <span className="px-4 py-[8px] uppercase text-sm border border-solid rounded-[80px] backdrop-blur-xl border-[rgba(255,255,255,.4)] bg-[#1b1b1d] transition-all">Contact Me</span>
             </div>
-            <h4 className={`${righteous.className} text-3xl lg:text-5xl mb-6`}>Let&#39;s Get In Touch!</h4>
-            <p className="font-light opacity-70 mb-6">
+            <h4 className={`${righteous.className} text-3xl lg:text-5xl mb-8`}>Let&#39;s Get In Touch!</h4>
+            <p className="font-light opacity-70 mb-8">
               If you&#39;re interested in learning more about me, collaborating on a project, or simply curious and want to say hello, please don&#39;t hesitate to send me a message. I enjoy receiving messages from people on the internet!
             </p>
-            <div className="border-t-0 border-b-[1px] border-solid border-[#454547] mb-6" />
-            <p className={`${righteous.className} text-lg mb-4`}>Follow Me</p>
+            <div className="border-t-0 border-b-[1px] border-solid border-[#454547] mb-8" />
+            <p className={`${righteous.className} text-xl mb-4`}>Follow Me</p>
             <div className="flex flex-wrap items-center gap-4">
-              <Link href="https://www.facebook.com/polashahmad01" target="_blank" className="transition-all hover:opacity-80">
-                <FaSquareFacebook size="25px" />
+              <Link href="https://www.facebook.com/polashahmad01" target="_blank" className="transition-all">
+                <FaSquareFacebook size="25px" className="transition-all hover:text-[#20bf55]" />
               </Link>
-              <Link href="https://twitter.com/polashahmad01" target="_blank" className="transition-all hover:opacity-80">
-                <FaTwitterSquare size="25px" />
+              <Link href="https://twitter.com/polashahmad01" target="_blank" className="transition-all">
+                <FaTwitterSquare size="25px" className="transition-all hover:text-[#20bf55]" />
               </Link>
-              <Link href="https://www.linkedin.com/in/polashahmad01" target="_blank" className="transition-all hover:opacity-80">
-                <FaLinkedin size="25px" />
+              <Link href="https://www.linkedin.com/in/polashahmad01" target="_blank" className="transition-all">
+                <FaLinkedin size="25px" className="transition-all hover:text-[#20bf55]" />
               </Link>
-              <Link href="https://wa.me/+8801795186140" target="_blank" className="transition-all hover:opacity-80">
-                <FaSquareWhatsapp size="25px" />
+              <Link href="https://wa.me/+8801795186140" target="_blank" className="transition-all">
+                <FaSquareWhatsapp size="25px" className="transition-all hover:text-[#20bf55]" />
               </Link>
             </div>
           </div>
@@ -48,7 +48,7 @@ export default function ContactPage() {
               <div className="w-full">
                 <textarea className="py-[12px] bg-transparent border-y-0 border-t-0 border-b-[1px] border-solid border-[#454547] w-full opacity-50 resize-none transition-all outline-none focus:border-[#20bf55] focus:opacity-80" rows={1} placeholder="Enter your message"></textarea>
               </div>
-              <button className="px-4 py-[14px] w-full flex items-center justify-center gap-2 bg-[#171717] rounded-xl border border-solid border-[rgba(255,255,255,.1)] font-semibold transition-all hover:border-[#20bf55] hover:rounded-xl hover:text-[#20bf55]">
+              <button className="px-4 py-[14px] w-full flex items-center justify-center gap-2 bg-[#171717] rounded-xl border border-solid border-[#20bf55] font-semibold text-[#20bf55] transition-all hover:bg-[#20bf55] hover:text-black">
                 <FaTelegramPlane size="20px" className="" />
                 <span>Send Message</span>
               </button>
